ab1d51841c934ccde75cc7a738f136d14762650c
Tomás Esteves <102005571+tomyesteves@users.noreply.github.com>
2023-07-06 01:04:09 -0300

    Add files via upload

---
+++ Scripts/killProcess.ps1
@@ -0,0 +1,12 @@
+﻿param(
+    [string]$procesoId
+)
+
+$procesos = Get-Process
+$procesoAEliminar = $procesos | Where-Object {$_.Id -eq $procesoId}
+if ($procesoAEliminar) {
+    Stop-Process -Id $procesoId -Force
+    Write-Host "Proceso eliminado correctamente"
+} else {
+    Write-Host "No se encontró ningún proceso con el ID especificado"
+}
---
+++ Scripts/pauseProcess.ps1
@@ -0,0 +1,12 @@
+﻿param(
+    [string]$procesoId
+)
+
+$procesos = Get-Process
+$procesoAPausar = $procesos | Where-Object {$_.Id -eq $procesoId}
+if ($procesoAPausar) {
+    Suspend-Process -Id $procesoId
+    Write-Host "Proceso pausado correctamente"
+} else {
+    Write-Host "No se encontró ningún proceso con el ID especificado"
+}
